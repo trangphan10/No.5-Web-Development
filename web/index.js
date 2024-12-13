bb63bc2e1dd4a9ddbb2849b5d95a6a9c0fcd3cb7
trung201202 <doanductrung_t65@hus.edu.vn>
2024-12-14 06:08:17 +0700

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,9 +1,1 @@
-$(function() {
-    $("#start-time, #end-time").timepicker({
-        timeFormat: "HH:mm",
-        interval: 30,
-        dynamic: false,
-        dropdown: true,
-        scrollbar: true
-    });
-});
+
