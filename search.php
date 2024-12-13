43889c10b47d89e0dbc9ea08ef5f2a75a66d68c5
hoangminhson-t66 <hoangminhson_t66@hus.edu.vn>
2024-12-13 19:06:57 +0700

    Add files via upload

---
+++ search.php
@@ -0,0 +1,10 @@
+<!-- search.php -->
+<?php
+//input database
+//search engine: 
+//for word in search_table:
+    //if word in database:
+        //return db[word]
+    //else:
+        //print("we do not find your search items")
+?>
