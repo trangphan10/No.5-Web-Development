b8e2b7ae5cff45d6f0226fdbc37060c627cabc50
NguyenMinhQuanK66A4 <148624103+NguyenMinhQuanK66A4@users.noreply.github.com>
2025-01-06 22:50:02 +0700

    Sửa file REQUEST

--- REQUEST.php
+++ REQUEST.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "user_management";
+    $dbname = "event_management";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
